f12e832a843470c31ac6987ae5918b5b9586408d
Fiona Gruell <Fiona@pc10.pool.tp1.uni-duesseldorf.de>
2015-12-15 12:27:10 +0100

    lab8

--- rel_osc.cxx
+++ rel_osc.cxx
@@ -6,8 +6,9 @@
 #include <iostream>
 #include <fstream>
 //--------------------
+void b(double& b1,double& b2,double& b3,double& b4,double Theta);
 void f(double* const y0, const double x);
-void RKstep(double* const yn, const double* const y0, const double x, const double dx);
+void RKstep(double* const yn, const double* const y0, const double x, const double dx,double* k1,double* k2,double* k3,double* k4);
 //--------------------
 using namespace std;
 //--------------------
@@ -15,29 +16,53 @@ using namespace std;
 int main(void)
 {
 	ofstream out("solution");
-  const int dim = 3;
-	double dx = 0.001,x=0;
-	const double L = 100;
-  double y0[dim] = {1.01, 1.0, 1.0};
+  const int dim = 2;
+	double dx = 0.3,x=0;
+	const double L = 20*M_PI;
+    double y0[dim],yi[dim];
+    
 	double yn[dim];
-
-  out << x << "\t" << y0[0] << "\t" << y0[1] << "\t" << y0[2] << endl;
+	double b1,b2,b3,b4;
+	double k1[dim], k2[dim], k3[dim], k4[dim];
+	
+ 	for(double j=0;j<5;j+=0.001){
+	 y0[0]=(1./10) +j;
+	 y0[1]=0;
+	x=0;
 	while(x<=L)
 	{
 		x += dx;
-		RKstep(yn, y0, x, dx);
-    for(int i=0; i<dim; i++) y0[i] = yn[i];
-		out << x << "\t" << y0[0] << "\t" << y0[1] << "\t" << y0[2] << endl;
+		RKstep(yn, y0, x, dx,k1,k2,k3,k4);
+          	if(yn[1]<0 && y0[1]>0) break;
+                for(int i=0; i<dim; i++) y0[i] = yn[i];
+		//out << x << "\t" << y0[0] << "\t" << y0[1] <<  endl;
+	}
+	double ThetaL=0,ThetaR=1,Theta=0.5;
+	yi[1]=1;
+       while(abs(yi[1])>1e-6)
+	{
+	  b(b1,b2,b3,b4,Theta);
+	  //yi[0]=y0[0]+dx*(b1*k1[0]+b2*k2[0]+b3*k3[0]+b4*k4[0]);
+	  yi[1]=y0[1]+dx*(b1*k1[1]+b2*k2[1]+b3*k3[1]+b4*k4[1]);
+	  if(yi[1]>0)
+	    ThetaL=Theta;
+	  else ThetaR=Theta;
+	  Theta=(ThetaL+ThetaR)/2.;
+	  //cout << Theta << "\t" << yi[1] << endl;
+	} 
+	
+	//cout << Theta << endl;
+ out <<(1./10) +j <<"\t"<<x+Theta*dx -dx <<"\t"<<y0[0]<<"\t"<<y0[1]<<endl;
 	}
 	out.close();
 	return(0);
-}
+	}
 //-------------------
 void RKstep(double* const yn, const double* const y0,
-            const double x, const double dx)
+            const double x, const double dx,double* k1,double* k2,double* k3,double* k4)
 {
-	const int dim = 3;
-	double k1[dim], k2[dim], k3[dim], k4[dim];
+	const int dim = 2;
+	
 
   for(int i=0;i<dim; i++) k1[i] = y0[i];
 	f(k1, x);
@@ -58,12 +83,16 @@ void RKstep(double* const yn, const double* const y0,
 // Lorenz model
 void f(double* const y0, const double x)
 {
-	const double a = 10;
-	const double b = 28;
-	const double c = 8.0/3.0;
-	double y[3] = { y0[0], y0[1], y0[2] };
+	double y[2] = { y0[0], y0[1] };
 
-  y0[0] = a*(y[1] - y[0]);
-	y0[1] = y[0]*(b - y[2]) - y[1];
-	y0[2] = y[0]*y[1] - c*y[2];
+  y0[0] = y[1];
+  y0[1] = -y[0]/(sqrt(1+pow(y[0],2)));
+	
+}
+void b(double& b1,double& b2,double& b3,double& b4,double Theta){
+b1= Theta-3*Theta*Theta/2.+2.*Theta*Theta*Theta/3.;
+b2=Theta*Theta-2.*Theta*Theta*Theta/3.;
+b3=b2;
+b4=-Theta*Theta/2.+2.*Theta*Theta*Theta/3.;
+  
 }
